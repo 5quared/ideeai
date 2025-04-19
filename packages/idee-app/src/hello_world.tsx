7e1258a809ea571ed626b8120613acd6f2a237b3
André Kohn <kohn.a@outlook.com>
2025-04-19 21:21:39 +0200

    Add example css module

--- packages/idee-app/src/hello_world.tsx
+++ packages/idee-app/src/hello_world.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
+import * as styles from './hello_world.module.css';
 
 export function HelloWorld(_props: {}) {
-    return <div>Hello World</div>;
+    return <div className={styles.hello_world}>Hello World</div>;
 }
