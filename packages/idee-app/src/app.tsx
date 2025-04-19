9e19633263457d5c495adcbda1fbf1327ed7fcd9
André Kohn <kohn.a@outlook.com>
2025-04-19 20:57:28 +0200

    Don't auto-navigate to root

--- packages/idee-app/src/app.tsx
+++ packages/idee-app/src/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import { HelloWorld } from './hello_world.js';
 
@@ -12,7 +12,6 @@ root.render(
     <Router>
         <Routes>
             <Route index Component={HelloWorld} />
-            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     </Router>
 );
